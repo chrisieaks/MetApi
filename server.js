1a627b0f9e455da2f3f1fe575a66c0ee50b63a6f
Chris <chrisieaks@gmail.com>
2018-03-23 11:05:20 -0400

    server boilerplate

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+const exphbs = require('express-handlebars');
+
+const db = require('./models');
+
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+require('./routes/api-routes.js')(app);
+require('./routes/html-routes')(app);
+
+db.sequlize.sync({ force: true }).then(function(){
+    app.listen(PORT, function(){
+        console.log(`App listening on PORT: ${PORT}`);
+    });
+});
+
+
