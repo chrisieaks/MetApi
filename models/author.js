ac5653ffbf24ec4685b810e7a71f8860ed226006
Chris <chrisieaks@gmail.com>
2018-03-26 14:39:33 -0400

    auth routing

--- models/author.js
+++ models/author.js
@@ -1,25 +1,26 @@
 
-var mysql = require('mysql');
+// var mysql = require('mysql');
 var Sequelize = require("sequelize");
 var config = require('../config/config.json');
 
-var connection = new Sequelize("metApi_db", "root", "",{
-    host: "localhost",
-    dialect: 'mysql',
-});
 
-var ApiData = connection.define("ApiData",{
-    name: Sequelize.STRING,
-    url: Sequelize.STRING,
-    tags: Sequelize.BOOLEAN,
-    free: Sequelize.BOOLEAN,
-});
 
-ApiData.associate = function(models){
-    ApiData.hasMany(models.Post,{
-        
-    })
-}
+
+
+
+
+
+// var ApiData = connection.define("ApiData",{
+//     name: Sequelize.STRING,
+//     url: Sequelize.STRING,
+//     tags: Sequelize.BOOLEAN,
+//     free: Sequelize.BOOLEAN,
+// });
+
+// ApiData.associate = function(models){
+//     ApiData.hasMany(models.Post,{
+//     })  
+// }
 
 
 // console.log("All Good")
