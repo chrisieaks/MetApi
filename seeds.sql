3d53885ff73d94da7ae4c52ecd6288a5a6ec7784
Ian Oh <ianoh@Ians-MacBook-Pro.local>
2018-03-29 10:44:08 -0400

    Updates seeds

--- seeds.sql
+++ seeds.sql
@@ -3,11 +3,92 @@ VALUES("Spotify", "https://developer.spotify.com/web-api/", true, true);
 
 --post--
 
+-- Social API 
 INSERT INTO Posts(name, url, tags, free, clientServer)
-VALUES("Google","www.google.com", true, true, "server");
+VALUES("Ebay","https://go.developer.ebay.com/", "social", true, "server");
 
 INSERT INTO Posts(name, url, tags, free, clientServer)
-VALUES("Ebay","www.ebay.com", true, true, "server");
+VALUES("Youtube","https://developers.google.com/youtube/", "social", true, "server");
 
 INSERT INTO Posts(name, url, tags, free, clientServer)
-VALUES("Youtube","www.youtube.com", true, true, "server");
+VALUES("Pinterest", "https://developers.pinterest.com/docs/getting-started/introduction/?", "social", true, "server");
+
+INSERT INTO Posts(name, url, tags, free, clientServer)
+VALUES("Crime Data", "https://crime-data-explorer.fr.cloud.gov/api", "social", true, "server");
+
+INSERT INTO Posts(name, url, tags, free, clientServer)
+VALUES("Facebook", "https://developers.facebook.com/", "social", true, "server");
+
+INSERT INTO Posts(name, url, tags, free, clientServer)
+VALUES("Twitter", "https://dev.twitter.com/rest/public", "social", true, "server");
+
+INSERT INTO Posts(name, url, tags, free, clientServer)
+VALUES("Foursquare", "https://developer.foursquare.com/", "social", true, "server");
+
+INSERT INTO Posts(name, url, tags, free, clientServer)
+VALUES("Meetup", "https://www.meetup.com/meetup_api/", "social", true, "server");
+
+INSERT INTO Posts(name, url, tags, free, clientServer)
+VALUES("Slack", "https://api.slack.com/", "social", true, "server");
+
+INSERT INTO Posts(name, url, tags, free, clientServer)
+VALUES("Flickr", "https://www.flickr.com/services/api/", "social", true, "server");
+
+INSERT INTO Posts(name, url, tags, free, clientServer)
+VALUES("Google Plus", "https://developers.google.com/+/web/api/rest/", "social", true, "server");
+
+-- Tool API
+INSERT INTO Posts(name, url, tags, free, clientServer)
+VALUES("Google App Engine","https://developers.google.com/appengine/", "tool", true, "server");
+
+INSERT INTO Posts(name, url, tags, free, clientServer)
+VALUES("Github","https://developer.github.com/v3/", "tool", true, "server");
+
+INSERT INTO Posts(name, url, tags, free, clientServer)
+VALUES("Google Fusion Tables","https://developers.google.com/fusiontables/", "tool", true, "server");
+
+INSERT INTO Posts(name, url, tags, free, clientServer)
+VALUES("uClassify","http://www.uclassify.com/", "tool", true, "server");
+
+INSERT INTO Posts(name, url, tags, free, clientServer)
+VALUES("COLOURlovers","http://www.colourlovers.com/api", "tool", true, "server");
+
+INSERT INTO Posts(name, url, tags, free, clientServer)
+VALUES("Pryv","http://api.pryv.com/", "tool", true, "server");
+
+INSERT INTO Posts(name, url, tags, free, clientServer)
+VALUES("Zapier","https://zapier.com/developer/", "tool", true, "server");
+
+INSERT INTO Posts(name, url, tags, free, clientServer)
+VALUES("Slack Web","https://api.slack.com/", "tool", true, "server");
+
+INSERT INTO Posts(name, url, tags, free, clientServer)
+VALUES("High Charts","http://api.highcharts.com/highcharts", "tool", true, "server");
+
+INSERT INTO Posts(name, url, tags, free, clientServer)
+VALUES("JSON Tools","https://www.mashape.com/thefosk/Json-Tools", "tool", true, "server");
+
+-- Data API
+INSERT INTO Posts(name, url, tags, free, clientServer)
+VALUES("Gather Data","https://gatherdata.co/docs/", "data", true, "server");
+
+INSERT INTO Posts(name, url, tags, free, clientServer)
+VALUES("BusinessUSA Data","http://business.usa.gov/developer#data", "data", true, "server");
+
+INSERT INTO Posts(name, url, tags, free, clientServer)
+VALUES("Youtube Data","https://developers.google.com/youtube/v3/", "data", true, "server");
+
+INSERT INTO Posts(name, url, tags, free, clientServer)
+VALUES("FuturePrice Data", "https://secure.futureprice.com/", "data", true, "server");
+
+INSERT INTO Posts(name, url, tags, free, clientServer)
+VALUES("Fantasy Sports Net","http://www.fantasysportnet.com/data/home.jsp", "data", true, "server");
+
+INSERT INTO Posts(name, url, tags, free, clientServer)
+VALUES("IllumiDesk Webhooks","https://developers.illumidesk.com/reference#service_trigger_create", "data", true, "server");
+
+INSERT INTO Posts(name, url, tags, free, clientServer)
+VALUES("Google Abusive Experience Report API","https://developers.google.com/abusive-experience-report/v1/reference/rest/", "data", true, "server");
+
+INSERT INTO Posts(name, url, tags, free, clientServer)
+VALUES("Press Association Olympics","https://olympics.pressassociation.io/reference", "data", true, "server");
